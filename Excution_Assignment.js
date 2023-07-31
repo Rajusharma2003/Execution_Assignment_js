02f392361464cdd9c5970dfac9a61a3a71b21b4b
Rajusharma2003 <rajusharma50154@gmail.com>
2023-07-31 21:19:07 +0530

    Execution in js

---
+++ Excution_Assignment.js
@@ -0,0 +1,64 @@
+
+
+// Ques 1 
+
+// console.log(addToNumber(3,4));
+
+function addToNumber( a ,b ){
+    return a+b
+}
+
+
+
+//  Ques 2 
+
+
+// console.log(multiplyTwoNumber( 4 , 6));
+
+const multiplyTwoNumber = function (num1 ,num2){
+    return num1* num2
+}
+
+
+// Ques 3
+
+function addToNumber2(a ,b ){
+
+    // console.log(sum);
+    var sum;
+    sum = a + b ;
+    // return sum
+}
+
+// console.log(addToNumber ( 4, 6 ));
+
+
+
+
+// Ques 4
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+var x = 'rr'
+let y = 'kk'
+const z = 'nn'
+
+
+
+// Ques 5 
+
+
+{
+    console.log(x);
+
+    let x = 2
+
+    console.log(x);
+}
+
+
+
+
